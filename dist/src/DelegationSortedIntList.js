4c1b657a5863cd316cb466bef0a6f830acbb98bf
Julia <jmwgraham5@gmail.com>
2021-09-29 10:55:31 -0400

    delegation

--- dist/src/DelegationSortedIntList.js
+++ dist/src/DelegationSortedIntList.js
@@ -13,22 +13,45 @@ var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (
     if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
     return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
 };
-var _DelegationSortedIntList_totalAdded;
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
+};
+var _DelegationSortedIntList_totalAdded, _DelegationSortedIntList_delegate;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DelegationSortedIntList = void 0;
+const SortedIntListLibrary_1 = require("./hidden/SortedIntListLibrary");
 // HINT: Take a look at the UML diagram to see what DelegationSortedIntList
 //       should implement.
 class DelegationSortedIntList {
     constructor() {
         // the number of attempted element insertions
-        _DelegationSortedIntList_totalAdded.set(this, 0
-        /**
-         * Gets the total number of attempted int insertions to the list since it.
-         * was created.
-         *
-         * @return total number of integers added to the list.
-         */
-        );
+        _DelegationSortedIntList_totalAdded.set(this, 0);
+        _DelegationSortedIntList_delegate.set(this, new SortedIntListLibrary_1.SortedIntList());
+    }
+    add(num) {
+        var _a;
+        let result = __classPrivateFieldGet(this, _DelegationSortedIntList_delegate, "f").add(num);
+        if (result)
+            __classPrivateFieldSet(this, _DelegationSortedIntList_totalAdded, (_a = __classPrivateFieldGet(this, _DelegationSortedIntList_totalAdded, "f"), _a++, _a), "f");
+        return result;
+    }
+    addAll(list) {
+        throw new Error("Method not implemented.");
+    }
+    get(index) {
+        throw new Error("Method not implemented.");
+    }
+    remove(num) {
+        throw new Error("Method not implemented.");
+    }
+    removeAll(list) {
+        throw new Error("Method not implemented.");
+    }
+    size() {
+        throw new Error("Method not implemented.");
     }
     /**
      * Gets the total number of attempted int insertions to the list since it.
@@ -41,5 +64,5 @@ class DelegationSortedIntList {
     }
 }
 exports.DelegationSortedIntList = DelegationSortedIntList;
-_DelegationSortedIntList_totalAdded = new WeakMap();
+_DelegationSortedIntList_totalAdded = new WeakMap(), _DelegationSortedIntList_delegate = new WeakMap();
 //# sourceMappingURL=DelegationSortedIntList.js.map
--- src/DelegationSortedIntList.ts
+++ src/DelegationSortedIntList.ts
@@ -8,11 +8,36 @@
  *
  */
 
+import { SortedIntList } from "./hidden/SortedIntListLibrary"
+import { IntegerList } from "./IntegerList"
+
 // HINT: Take a look at the UML diagram to see what DelegationSortedIntList
 //       should implement.
-class DelegationSortedIntList {
+class DelegationSortedIntList implements IntegerList {
     // the number of attempted element insertions
     #totalAdded: number = 0
+    #delegate: SortedIntList = new SortedIntList()
+    
+    add(num: number): boolean {
+        let result: boolean = this.#delegate.add(num);
+        if(result) this.#totalAdded++;
+        return result
+    }
+    addAll(list: IntegerList): boolean {
+        throw new Error("Method not implemented.");
+    }
+    get(index: number): number {
+        throw new Error("Method not implemented.");
+    }
+    remove(num: number): boolean {
+        throw new Error("Method not implemented.");
+    }
+    removeAll(list: IntegerList): boolean {
+        throw new Error("Method not implemented.");
+    }
+    size(): number {
+        throw new Error("Method not implemented.");
+    }
 
     /**
      * Gets the total number of attempted int insertions to the list since it.
